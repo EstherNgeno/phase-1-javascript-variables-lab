1ba9271110ed1805f00319b7dd0d55cb8dc3fe3c
Esther <esther.ngeno@student.moringaschool.com>
2024-01-12 15:30:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let companyName= 'Scuber';
+companyName= 'Scuber';
+console.log (companyName);
 // Code your solution in this file!
+const companyName= 'Scuber';
+
+let mostProfitableNeighborhood= 'Chelsea';
+mostProfitableNeighborhood= 'Chelsea';
+console.log (mostProfitableNeighborhood);
+
+let companyCeo= 'Susan Smith';
+companyCeo= 'Susan Smith';
+console.log (companyCeo);
+
