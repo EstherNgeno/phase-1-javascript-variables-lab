402de042ad92e46da0145b6e2649dfdb1ecf79dd
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-06-27 21:09:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-let companyName= 'Scuber';
-companyName= 'Scuber';
-console.log (companyName);
-// Code your solution in this file!
 const companyName= 'Scuber';
-
-let mostProfitableNeighborhood= 'Chelsea';
-mostProfitableNeighborhood= 'Chelsea';
-console.log (mostProfitableNeighborhood);
-
-let companyCeo= 'Susan Smith';
-companyCeo= 'Susan Smith';
-console.log (companyCeo);
-
+let mostProfitableNeighborhood= "Chelsea";
+let companyCeo= "Susan Smith";
